3c3608f498c6783fdf6352cdd5109b561ff32b4b
surafelTehulu <surafeltehulu0910@gmail.com>
2023-07-13 07:19:07 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+
 /**
-*main - main entry point
-*return : always 0 (success)
+* main - Entry point of the program
+*
+* Description: Prints a message to the console using the puts function
+*
+* Return: Always 0 (success)
 */
 int main(void)
 {
