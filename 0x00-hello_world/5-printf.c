208e3506a481153610e9e4b4871f77429d8d73ab
surafelTehulu <surafeltehulu0910@gmail.com>
2023-07-13 07:26:47 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+*
+* Description: Prints a message to the console using the puts function
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+
+}
